799dd4e91085c9cc450e4df8f0907e3281a9cba0
raviraj060508-spec <raviraj060508@gmail.com>
2026-03-15 19:10:13 +0530

    Implement AI Mental Wellness Analyzer with predictions

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+st.title("AI Mental Wellness Analyzer")
+
+st.write("Predict stress level based on behavior.")
+
+typing_speed = st.slider("Typing Speed",20,100,50)
+typing_errors = st.slider("Typing Errors",0,10,2)
+screen_time = st.slider("Screen Time (hours)",0,10,3)
+
+X = np.array([
+[65,1,2],
+[40,8,6],
+[55,3,3],
+[35,10,7],
+[70,1,1]
+])
+
+y = np.array([0,2,1,2,0])
+
+model = RandomForestClassifier()
+model.fit(X,y)
+
+if st.button("Predict Stress"):
+    
+    prediction = model.predict([[typing_speed,typing_errors,screen_time]])
+
+    if prediction == 0:
+        st.success("Relaxed")
+    elif prediction == 1:
+        st.warning("Moderate Stress")
+    else:
+        st.error("High Stress")import streamlit as st
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+st.title("AI Mental Wellness Analyzer")
+
+st.write("Predict stress level based on behavior.")
+
+typing_speed = st.slider("Typing Speed",20,100,50)
+typing_errors = st.slider("Typing Errors",0,10,2)
+screen_time = st.slider("Screen Time (hours)",0,10,3)
+
+X = np.array([
+[65,1,2],
+[40,8,6],
+[55,3,3],
+[35,10,7],
+[70,1,1]
+])
+
+y = np.array([0,2,1,2,0])
+
+model = RandomForestClassifier()
+model.fit(X,y)
+
+if st.button("Predict Stress"):
+    
+    prediction = model.predict([[typing_speed,typing_errors,screen_time]])
+
+    if prediction == 0:
+        st.success("Relaxed")
+    elif prediction == 1:
+        st.warning("Moderate Stress")
+    else:
+        st.error("High Stress")
